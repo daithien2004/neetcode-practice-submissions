aa77260ec06bac6d985df284f982a1e760dd5559
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-04 15:30:07 +0700

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let freq = new Array(26).fill(0);
+        let l = 0, maxFreq = 0, result = 0;
+
+        for (let r=0; r<s.length; r++) {
+            freq[s.charCodeAt(r)-65]++;
+            maxFreq=Math.max(maxFreq, freq[s.charCodeAt(r)-65]);
+
+            if (r-l+1-maxFreq>k) {
+                freq[s.charCodeAt(l)-65]--;
+                l++;
+            }
+
+            result = Math.max(result, r-l+1);
+        }
+        
+        return result;
+    }
+}
