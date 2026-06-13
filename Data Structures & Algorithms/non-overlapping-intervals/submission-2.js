bc69ea3f228c3880b5791f700212154713531486
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-13 11:19:32 +0700

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        intervals.sort((a,b)=>a[1]-b[1])
+        let count = 0, prevEnd = -Infinity
+        for (const [start, end] of intervals) {
+            if (start >= prevEnd) prevEnd = end
+            else count++
+        }
+        return count;
+    }
+}
