1a2dde133e51e5bcec3907a3cc8ae6d2bab6ab2f
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-13 15:27:34 +0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let a = 1, b = 1;
+
+        for (let i=2; i<=n; i++) {
+            [a, b] = [b, a + b]
+        }
+
+        return b;
+    }
+}
