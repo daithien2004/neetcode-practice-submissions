080de1746544f9627a85a7cf2c85cabc5a5f6cdf
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-13 11:31:32 +0700

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        if (!q && !p) return true;
+        if (!q || !p || q.val !== p.val)
+            return false
+        return this.isSameTree(q.left, p.left) && this.isSameTree(q.right, p.right)
+    }
+}
