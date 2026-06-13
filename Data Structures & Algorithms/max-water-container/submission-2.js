2a723da364d0e14c2f508be21218088ab918e1b4
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-13 15:10:48 +0700

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let l = 0, r = heights.length-1;
+        let re = 0;
+        while (l<r) {
+            re = Math.max(Math.min(heights[l], heights[r])*(r-l), re)
+            if (heights[l]<heights[r]) {
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+        
+        return re;
+    }
+}
