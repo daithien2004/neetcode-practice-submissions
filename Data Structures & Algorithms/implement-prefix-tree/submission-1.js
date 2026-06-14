4233290a0dd9ea9da9af6a3939a4080cdbe1bb23
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-14 16:21:42 +0700

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.js
@@ -0,0 +1,53 @@
+class TrieNode {
+    constructor() {
+        this.children = {}
+        this.isEnd = false
+    }
+}
+
+class PrefixTree {
+    constructor() {
+        this.root = new TrieNode()
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let node = this.root
+        for (const ch of word) {
+            if (!node.children[ch]) {
+                node.children[ch] = new TrieNode()
+            }
+            node = node.children[ch]
+        }
+        node.isEnd = true
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let node = this.root
+        for (const ch of word) {
+            if (!node.children[ch]) return false
+            node = node.children[ch]
+        }
+        return node.isEnd
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let node = this.root
+        for (const ch of prefix) {
+            if (!node.children[ch]) return false
+            node = node.children[ch] 
+        }
+        return true
+    }
+}
