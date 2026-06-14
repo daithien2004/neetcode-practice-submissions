c01dfc04e5259e23194da5a7b618135a0f92fb6b
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-14 13:07:11 +0700

    Add: binary-tree-maximum-path-sum - submission-1

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-1.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxPathSum(root) {
+        let max = -Infinity
+
+        function gain(node) {
+            if (!node) return 0
+            const l = Math.max(0, gain(node.left))
+            const r = Math.max(0, gain(node.right))
+            max = Math.max(max, node.val + r + l)
+            return node.val + Math.max(l, r)
+        }
+
+        gain(root)
+        return max
+    }
+}
