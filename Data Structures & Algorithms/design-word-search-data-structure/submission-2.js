a20883be31db07e09a211fdf7768bf7d9daf12e7
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-14 13:04:22 +0700

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.js
@@ -0,0 +1,48 @@
+class TrieNode {
+    constructor() {
+        this.children = {}
+        this.isEnd = false
+    }
+}
+
+class WordDictionary {
+    constructor() {
+        this.root = new TrieNode()
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word) {
+        let node = this.root
+        for (const ch of word) {
+            if (!node.children[ch]) {
+                node.children[ch] = new TrieNode()
+            }
+            node = node.children[ch]
+        }
+        node.isEnd = true
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        function dfs(node, i) {
+            if (i === word.length) return node.isEnd
+            const ch = word[i]
+            if (ch !== '.') {
+                const next = node.children[ch]
+                return next ? dfs(next, i+1) : false
+            }
+            for (const next of Object.values(node.children)) {
+                if (dfs(next, i+1)) return true
+            }
+            return false
+        }
+
+        return dfs(this.root, 0)
+    }
+}
