0a38db286d6a5a93a6a6f77afdb82ecc9342ea9c
daithien <147059883+daithien2004@users.noreply.github.com>
2026-06-15 15:09:36 +0700

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        const result = []
+
+        function backtrack(remaining, start, path) {
+            if (remaining === 0)
+                result.push([...path])
+
+            if (remaining < 0) return
+
+            for (let i = start; i<nums.length; i++) {
+                path.push(nums[i])
+                backtrack(remaining-nums[i], i, path)
+                path.pop(nums[i])
+            }
+        }
+    
+        backtrack(target, 0, [])
+        return result;
+    }
+}
